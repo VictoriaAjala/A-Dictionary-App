ce5f223f7881cb98fb1be102777d8c64b3d8db9b
VictoriaAjala <victoria.o.ajala@gmail.com>
2022-09-19 13:20:41 +0100

    Added Api in js

---
+++ script.js
@@ -0,0 +1,18 @@
+const wrapper = document.querySelector(".wrapper"),
+  searchInput = wrapper.querySelector("input");
+
+//fetch api function
+function fetchApi(word) {
+  infoText.style.color = "#000";
+  infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((result) => console.log(result));
+}
+
+searchInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter" && e.target.value) {
+    fetchApi(e.target.value);
+  }
+});
